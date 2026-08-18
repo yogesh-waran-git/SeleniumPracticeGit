c3c4db5e9dfad6b7d916a1c8a62e685ca81940ef
yogesh-waran-git <sivayogesh97@gmail.com>
2026-08-18 13:24:37 +0530

    Changes

--- src/test/java/org/sel/pr/Basic.java
+++ src/test/java/org/sel/pr/Basic.java
@@ -2,7 +2,13 @@ package org.sel.pr;
 
 public class Basic {
 	private void newMethod() {
+		int a=1;
+		int b=2+3;
+		
 		// TODO Auto-generated method stub
 
+	}
+	public void testMethod(){
+		
 	}
 }
